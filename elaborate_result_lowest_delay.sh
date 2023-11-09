9334ab56e9e54290d5747af4e5cc47ac5638f941
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2023-11-09 15:20:46 +0100

    Update and rename print_data_1m.sh to elaborate_result_lowest_delay.sh

--- elaborate_result_lowest_delay.sh
+++ elaborate_result_lowest_delay.sh
@@ -7,12 +7,7 @@ start_time=$(date +%s)
 
 
 while true; do
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
 
-    if [ $elapsed_time -ge $total_duration ]; then
-        break  
-    fi
     python3 Tesi/data_processing_1m.py
     python3 Tesi/invert_csv.py
     python3 Tesi/data_extrapolation.py
