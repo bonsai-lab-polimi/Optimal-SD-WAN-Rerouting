343e61a3a0680a84b671665eb977479bfbe4baa4
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2023-11-09 15:19:34 +0100

    Update and rename delay.sh to delay1.sh

--- delay1.sh
+++ delay1.sh
@@ -1,20 +1,9 @@
 #! usr/bin/bash
 
-total_duration=3600
-
-
-start_time=$(date +%s)
 
 mininet/util/m s3 tc qdisc add dev s3-eth1 root netem delay 50ms 5ms 25% distribution pareto
 
 while true; do
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
-
-    if [ $elapsed_time -ge $total_duration ]; then
-        break  
-    fi
-
     mininet/util/m s2 tc qdisc add dev s2-eth1 root netem delay 25ms 2ms 25% distribution pareto
     sleep 20s
     mininet/util/m s2 tc qdisc del dev s2-eth1 root 
@@ -37,8 +26,6 @@ while true; do
     sleep 20s
     mininet/util/m s2 tc qdisc del dev s2-eth1 root 
     sleep 90s
-
-
 
 done
 
