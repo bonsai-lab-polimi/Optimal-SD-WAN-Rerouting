89a43a85f98edac2e2fc5a5c05a1af257b2c9964
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2023-11-09 15:18:31 +0100

    Update data_processing.py

--- data_processing_1m.py
+++ data_processing_1m.py
@@ -15,7 +15,7 @@ client = InfluxDBClient(url=url, token=token, org=org)
 
 
 query ="""from(bucket:"Poli")
-  |> range(start: -3m, stop: now())
+  |> range(start: -20s, stop: now())
   |> filter(fn: (r) => r["url"] == "10.0.0.4" or r["url"] == "10.0.0.5" or r["url"] == "10.0.0.6" or r["url"] == "10.0.0.7")
   |> filter(fn: (r) => r["_field"] == "average_response_ms")
   |> filter(fn: (r) => r["_measurement"] == "ping")
@@ -27,7 +27,4 @@ query ="""from(bucket:"Poli")
   
   
 result = client.query_api().query_data_frame(org=org, query=query)
-#print(result[0])
-#result=pd.concat(result, ignore_index=True)
-#print("----------------------------------")
 result[0].to_csv('Tesi/out6.csv', index=False, columns=result[0].columns) 
